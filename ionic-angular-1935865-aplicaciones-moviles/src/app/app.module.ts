d203f53589a79c2db1bb1bf94ba74083c6c1cb0e
Sergio-Gtz-V <Sergio-Gtz-V@github.com>
2022-05-28 22:50:57 -0500

    Creacion de Primera APK Funcional

--- ionic-angular-1935865-aplicaciones-moviles/src/app/app.module.ts
+++ ionic-angular-1935865-aplicaciones-moviles/src/app/app.module.ts
@@ -6,14 +6,27 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { FirebaseError, initializeApp } from 'firebase/app';
+//import { FirebaseError, initializeApp} from 'firebase/app';
 import { environment } from 'src/environments/environment';
+//import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+//import { provideStorage, getStorage } from '@angular/fire/storage';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule, 
+    FormsModule, 
+    HttpClientModule
+    // provideStorage(() => getStorage()), 
+    // provideFirebaseApp( () => initializeApp(environment.firebaseConfig)) 
+  
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  
 })
 export class AppModule {}
